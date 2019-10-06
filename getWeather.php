4089bcf351c24df6b617ea134981521928f57e2c
Will <wilmc17@gmail.com>
2019-10-05 18:19:01 -0700

    Removed api keys from public view (old keys no longer work)

--- getWeather.php
+++ getWeather.php
@@ -4,7 +4,7 @@ $lat = $_REQUEST["lat"];
 $long = $_REQUEST["long"];
 
 //Get timezone from coordinates
-$tzRequest = file_get_contents("https://maps.googleapis.com/maps/api/timezone/json?location=".$lat.",".$long."&timestamp=".time()."&key=AIzaSyCgrdNnFg9NT18fRHs6wlFKdFlXoGHm7Tg");
+$tzRequest = file_get_contents("https://maps.googleapis.com/maps/api/timezone/json?location=".$lat.",".$long."&timestamp=".time()."&key=");//Google places API key here
 $tzJson = json_decode($tzRequest);
 date_default_timezone_set($tzJson->timeZoneId);
 
@@ -12,7 +12,7 @@ date_default_timezone_set($tzJson->timeZoneId);
 $hour = date('H');
 
 //Get weather from coordinates
-$weatherFile = file_get_contents("http://api.openweathermap.org/data/2.5/weather?lat=".$lat."&lon=".$long."&units=metric&appid=d935043beae3f8840734f335cfa43026");
+$weatherFile = file_get_contents("http://api.openweathermap.org/data/2.5/weather?lat=".$lat."&lon=".$long."&units=metric&appid=");//open weather API key here
 $weatherJson = json_decode($weatherFile);
 $maind = $weatherJson->weather[0]->main;
 
